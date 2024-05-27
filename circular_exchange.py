c4a60f9b5432503e81914ed418f8353014e12c41
Sean <sean.oshea@free.fr>
2024-05-27 13:56:23 +0200

    First implementation of the networkx package, creation of a MultiDiGraph and finding a cycle in it

--- circular_exchange.py
+++ circular_exchange.py
@@ -1,4 +1,8 @@
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+import itertools as it
+
 
 def get_item_by_id(items, item_wished):
     for item in items:
@@ -6,40 +10,46 @@ def get_item_by_id(items, item_wished):
             return item
     return None
 
-
 f_users = open('tests/users.json')
 f_items = open('tests/items.json')
 
 users = json.load(f_users)
 items = json.load(f_items)
 
-# create a graph with all the users, linked together based on their wishes
-users_items = {}
-for item in items:
-    if users_items.get(item['user_id']) is None:
-        users_items[item['user_id']] = []
+# create a directed graph that can store multiple edges with all the users, linked together based on their wishes
+G = nx.MultiDiGraph()
 
-    # find all the users wishing to get this item
-    wished_by = [user['id'] for user in users if item['id'] in user['items_wishes_id']]
-
-    # link the owner of the item with the interested users, weighted by its value
-    users_items[item['user_id']].append({'weight': item['value'], 'wished_by': wished_by})
-
-users_graph = {}
 for user in users:
     if len(user['items_wishes_id']) > 0:
-        users_graph[user['id']] = []
         for item_wished_id in user['items_wishes_id']:
             item_wished = get_item_by_id(items, item_wished_id)
-            users_graph[user['id']].append({
-                'owner_id': item_wished['user_id'],
-                'weight': item_wished['value'],
-                'item_wished_id': item_wished['id']
-            })
+            G.add_edge(user['id'], item_wished['user_id'], weight=item_wished['value'], name=item_wished['name'])
 
-#
-print(users_graph)
+pos = nx.planar_layout(G)
+# add a curve to make it possible to visualize multiple edges between two nodes
+connectionstyle = "arc3,rad=0.4"
 
+nx.draw_networkx_nodes(G, pos, node_color="lightblue", node_size=300)
+nx.draw_networkx_edges(G, pos, edge_color="grey", connectionstyle=connectionstyle)
+nx.draw_networkx_labels(G, pos, font_size=12, font_family="sans-serif")
+
+labels = {
+        tuple(edge): f"{'val'}={attrs['weight']} - {attrs['name']}"
+        for *edge, attrs in G.edges(keys=True, data=True)
+    }
+nx.draw_networkx_edge_labels(
+    G, pos, edge_labels=labels,
+    connectionstyle=connectionstyle
+)
+
+# Find a cycle in the graph, value of item not taken into account
+cycle = nx.find_cycle(G, orientation="original")
+print(cycle)
+
+# Highlight the cycle in red
+nx.draw_networkx_edges(G, pos, edgelist=cycle, edge_color="r", width=2)
+
+plt.show()
 
 f_users.close()
 f_items.close()
